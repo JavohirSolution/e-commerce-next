4b8b12a4867ec3a0b5fe6c69c6c6ab8acd45a2df
JavohirSolution <javohirdeveloper12@gmail.com>
2024-08-26 17:41:17 +0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: '**',
-        }, {
-            protocol: 'http',
-            hostname: '**',
-        }]
-    }
+        remotePatterns: [
+            { protocol: "https", hostname: "*" },
+            { protocol: "http", hostname: "*" },
+        ],
+    },
 };
 
 export default nextConfig;
